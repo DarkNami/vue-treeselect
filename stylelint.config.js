90f7f687e3f539d78449f71ce4bc1c4d79003279
DarkNami <DarkNami@users.noreply.github.com>
2022-08-17 19:26:04 +0800

    npm: Update packages, upgrade 'stylelint'

--- stylelint.config.js
+++ stylelint.config.js
@@ -8,13 +8,16 @@ module.exports = {
     'rule-empty-line-before': null,
     'value-keyword-case': null, // [ 'lower', { ignoreProperties: [ 'font', 'font-family' ] } ],
     'declaration-block-no-duplicate-properties': [ true, { ignore: [ 'consecutive-duplicates' ] } ],
-    'declaration-property-value-blacklist': null,
-    'property-blacklist': null,
+    'declaration-property-value-disallowed-list': null,
+    'property-disallowed-list': null,
     'no-unknown-animations': null,
     'font-weight-notation': null,
     'no-descending-specificity': null,
     'selector-max-compound-selectors': null,
     'block-no-empty': [ true, { ignore: [] } ],
     'linebreaks': (process.platform === 'win32' ? 'windows' : 'unix'),
+    'alpha-value-notation': null,
+    'color-function-notation': null,
+    'function-no-unknown': null
   },
 }
