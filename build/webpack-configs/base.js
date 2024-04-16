26957f6b4b2fd64eb995cb29b406de4d2bf4631e
DarkNami <DarkNami@users.noreply.github.com>
2024-04-16 20:28:06 +0800

    build: fix webpack issue for /docs

--- build/webpack-configs/base.js
+++ build/webpack-configs/base.js
@@ -24,6 +24,7 @@ module.exports = {
     rules: [
       {
         test: /\.vue$/,
+        resourceQuery: { not: [/raw/] },
         loader: 'vue-loader',
         options: {
           compilerOptions: {
